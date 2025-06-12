f88df9d3808f02ab393db8b1c8495078b4367784
kmrr-nikhil <nikhil4u43@gmail.com>
2025-06-12 23:38:05 +0530

    update

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -11,7 +11,7 @@ const TODO_ITEMS = [
 ] as const;
 
 test.describe('New Todo', () => {
-  test('should allow me to add todo items', async ({ page }) => {
+  test('should allow me to add todo items in the list', async ({ page }) => {
     // create a new todo locator
     const newTodo = page.getByPlaceholder('What needs to be done?');
 
